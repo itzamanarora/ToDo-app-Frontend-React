9bf9f309083c25c78f2c66a961f02893f77dc470
itzamanarora <amanarora99005@gmail.com>
2026-09-14 12:33:46 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' 
 import tailwindcss from '@tailwindcss/vite' 
 
-export default defineConfig({ plugins: [react(), tailwindcss()], })
+export default defineConfig({ 
+    base:  '/my-react-app/',
+    plugins: [react(), tailwindcss()], })
